b16d2a3f9da08727f3507a56c7da734b10a7c795
firozt <firozt03@gmail.com>
2024-01-23 14:54:28 +0000

    added gluestacks integration

--- client/App.tsx
+++ client/App.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { View, Button, Text } from 'react-native';
+import { } from 'react-native';
 import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
 import { IOS_GOOGLE_LOGIN_ID, WEB_GOOGLE_ID } from '@env';
-
-
+import { GluestackUIProvider, GluestackUIStyledProvider, Text, Button, View, ButtonText, Switch, AlertDialog } from "@gluestack-ui/themed"
+import { config } from "@gluestack-ui/config"
 
 GoogleSignin.configure({
   webClientId: WEB_GOOGLE_ID,
@@ -32,11 +32,21 @@ export default function App() {
     }
   };
 
+  const [showAlert, setShowAlert] = React.useState<boolean>(false);
+  
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>{user}</Text>
-      <Text>Hello World!</Text>
-      <Button title="Sign in with Google" onPress={() => signIn()} />
-    </View>
+    <GluestackUIStyledProvider config={config}>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>{user}</Text>
+        <Text>Hello World!</Text>
+        <Button onPress={() => signIn()} >
+          <Text color='white'>Sign in with google</Text>
+        </Button>
+        <AlertDialog isOpen={true}/>
+        <Switch size="md" isDisabled={false}  />
+      
+      </View>
+    </GluestackUIStyledProvider>
   );
 }
